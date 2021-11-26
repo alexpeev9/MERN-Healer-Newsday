4e5630734b094abd80076f2d0cc344df140c7961
alexpeev9 <alexpeev9@abv.bg>
2021-11-26 23:06:37 +0200

    Add Error Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 
+import Error from './pages/Error';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -16,6 +17,7 @@ function App() {
          <Route path="/" element={<Home />} />
          <Route path="/login" element={<Login />} />
          <Route path="/register" element={<Register />} />
+         <Route path="*" element={<Error />} />
        </Routes>
      </main>
      <Footer />
---
+++ client/src/pages/Error/Error.js
@@ -0,0 +1,7 @@
+const Error = () => {
+    return (
+       <h1 className="text-danger text-center m-3">Error 404</h1>
+    );
+}
+
+export default Error;
