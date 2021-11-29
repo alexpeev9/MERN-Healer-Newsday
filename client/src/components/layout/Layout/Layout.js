5120a6443330f709b9c0361b878fa942a9adfe50
alexpeev9 <alexpeev9@abv.bg>
2021-11-29 17:26:54 +0200

    Setup Context for Greeting Message

--- client/src/components/layout/Layout/Layout.js
+++ client/src/components/layout/Layout/Layout.js
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react"
+
+import { UserContext } from "../../../utils/Context"
 import Header from "../Header"
 import Router from "../Router"
 import Footer from "../Footer"
+import { getUsernameCookie } from '../../../utils/cookieUtils.js';
 
 const Layout = () => {
+    const [username, setUsername] = useState(getUsernameCookie());
+    useEffect(() => {
+		try {
+			setUsername(getUsernameCookie());
+		} catch (e) {
+			setUsername(null);
+		}
+	}, [username])
     return (
-        <>
-            <Header />
-                <Router />
+        <UserContext.Provider value={[username, setUsername]}>
+            <Header user={username}/>
+            <Router />
             <Footer />
-        </>
+        </UserContext.Provider >
     );
 }
 
