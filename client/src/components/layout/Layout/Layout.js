30f44ce25848546a9d4011991525d9fd6570fb89
alexpeev9 <alexpeev9@abv.bg>
2021-12-03 15:11:00 +0200

    Add GlobalError To Layout

--- client/src/components/layout/Layout/Layout.js
+++ client/src/components/layout/Layout/Layout.js
@@ -1,25 +1,42 @@
 import { useEffect, useState } from "react"
 
-import { UserContext } from "../../../utils/Context"
+import { UserContext, ErrorContext } from "../../../utils/Context"
+import GlobalError from "../../error/GlobalError"
 import Header from "../Header"
 import Router from "../Router"
 import Footer from "../Footer"
 import { getUsernameCookie } from '../../../utils/cookieUtils.js';
 
+let timeout = 0
+
 const Layout = () => {
     const [username, setUsername] = useState(getUsernameCookie());
+    const [error, setError] = useState('');
+
     useEffect(() => {
-		try {
-			setUsername(getUsernameCookie());
-		} catch (e) {
-			setUsername(null);
-		}
-	}, [username])
+        try {
+            setUsername(getUsernameCookie());
+        } catch (e) {
+            setUsername(null);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (error !== '') {
+            timeout = setTimeout(() => setError(''), 8000)
+        } else {
+            clearTimeout(timeout)
+        }
+    }, [error, username])
+
     return (
         <UserContext.Provider value={[username, setUsername]}>
-            <Header user={username}/>
-            <Router />
-            <Footer />
+            <ErrorContext.Provider value={[error, setError]}>
+                <Header user={username} />
+                <GlobalError error={error} />
+                <Router />
+                <Footer />
+            </ErrorContext.Provider >
         </UserContext.Provider >
     );
 }
