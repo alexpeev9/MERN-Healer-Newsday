edebdc9420e3ae1a16051f6bdf2bb2a706d3af68
alexpeev9 <alexpeev9@abv.bg>
2021-12-03 15:44:44 +0200

    Change Register Service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -6,13 +6,6 @@ export const login = async (userData) => {
 };
 
 export const register = async (userData) => {
-    let response = await fetch(`${process.env.REACT_APP_BASE_URL} + "/api/user/register`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify(userData)
-    });
-    let result = await response.json();
-    return result;
+    let response = await serviceFetch('/user/register', 'POST', userData);
+    return response;
 };
