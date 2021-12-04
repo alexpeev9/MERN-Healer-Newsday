ddc1fc2905433487d4f9cc6eb07834e3d387aefe
alexpeev9 <alexpeev9@abv.bg>
2021-12-04 18:59:36 +0200

    Temporary removed StrictMode

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Layout from '../src/components/layout/Layout';
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Layout />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Layout />,document.getElementById('root')
 );
