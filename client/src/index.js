74052389dfc045a5412a08d98fb7986961b7fe07
alexpeev9 <alexpeev9@abv.bg>
2021-12-05 15:57:21 +0200

    Style StartUp

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import Layout from '../src/components/layout/Layout';
 
 ReactDOM.render(
-  <Layout />,document.getElementById('root')
+    <Layout />, document.getElementById('root')
 );
