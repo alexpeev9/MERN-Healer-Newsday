f0aa231ce160c00d8a2ba512d0acf8c250747de3
alexpeev9 <alexpeev9@abv.bg>
2021-12-05 20:29:18 +0200

    Style Index

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './.env' });
 
 const port = process.env.PORT;
 
 const getConnectionString = () => {
-    if(process.env.NODE_ENV == "production")
-    {
+    if (process.env.NODE_ENV == "production") {
         return process.env.DB_HOST_PROD;
     }
-    else{
+    else {
         return process.env.DB_HOST_DEV;
     }
 }
@@ -21,13 +19,12 @@ const { initDatabase } = require('./config/databaseConfig');
 const app = express();
 
 require('./config/expressConfig')(app);
-
 app.use(routes);
 
 initDatabase(getConnectionString())
-        .then(() => {
-            app.listen(port, () => console.log(`The app is running on http://localhost:${port}`));
-        })
-        .catch(err => {
-            console.log('Cannot connect to database', err);
-        })
+    .then(() => {
+        app.listen(port, () => console.log(`The app is running on http://localhost:${port}`));
+    })
+    .catch(err => {
+        console.log('Cannot connect to database', err);
+    })
