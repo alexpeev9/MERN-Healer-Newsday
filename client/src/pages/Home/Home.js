8bfbebf6a3f0a0c595bf0145ecd4a0006a39e17b
alexpeev9 <alexpeev9@abv.bg>
2021-12-22 17:42:58 +0200

    fix small changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,9 +5,9 @@ const Home = () => {
         <>
             {/* <img src={process.env.PUBLIC_URL + '/images/HealerFull.png'} alt="logo" /> */}
             <ListArticle />
-            <div class="container">
-                <div class="row">
-                    <section class="col-sm-12 maincontent">
+            <div className="container">
+                <div className="row">
+                    <section className="col-sm-12 maincontent">
                         <h3>Information about our site:</h3>
                         <p>Standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                         <h3>What you should know about:</h3>
